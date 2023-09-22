e1519cdd218338ffc7b21151de8ed2ed14dc45e9
hyunsik2000 <rhsik2000@gmail.com>
2023-09-22 15:59:51 +0900

    Activity Lifecycle

--- app/src/main/java/kr/ac/kumoh/ce/s20190400/s23w03lotto/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190400/s23w03lotto/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.ac.kumoh.ce.s20190400.s23w03lotto
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Lifecycle!!!", "onCreate()")
         // setContentView(R.layout.activity_main)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
@@ -29,5 +31,29 @@ class MainActivity : AppCompatActivity() {
             main.num6.text = Random.nextInt(1,46).toString()
 
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle!!!", "onStart()")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle!!!", "onResume()")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle!!!","onPause()")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle!!!","onRestart()")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle!!!","onStop()")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle!!!","onDestroy()")
     }
 }
